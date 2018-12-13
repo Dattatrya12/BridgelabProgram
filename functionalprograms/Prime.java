6d72555fc32e84f3e7e0f586c57042b7d3843804
Admin <dattaage999@gmail.com>
2018-12-13 18:28:55 +0530

    first commit

---
+++ functionalprograms/Prime.java
@@ -0,0 +1,20 @@
+package com.bridgelabz.functionalprograms;
+
+import com.utility.Utility;
+
+public class Prime {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		    System.out.println("Enter any positive number: ");
+				int n = Utility.getInt();
+				Utility.getPrime(n);
+				
+
+			}
+			
+
+
+	}
+
+
